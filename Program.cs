4fa8f8df0f1b3f9cca0eeda77074be762b7811a1
rakirs <srikarmylavarapu@gmail.com>
2022-11-25 13:48:30 -0600

    simple updates

--- Program.cs
+++ Program.cs
@@ -1,19 +1,9 @@
-﻿Console.WriteLine("Do you want to play a game of Tic-Tac-Toe");
+﻿using ConsoleTicTacToe;
+
+Console.WriteLine("Do you want to play a game of Tic-Tac-Toe");
 
 Console.WriteLine("X goes first, positions are listed as follows");
-
-var board = """ 
-         a     b     c
-            |     |     
-      1  1a |  1b |  1c  
-       _____|_____|_____
-            |     |     
-      2  2a |  2b |  2c 
-       _____|_____|_____
-            |     |     
-      3  3a |  3b |  3c  
-            |     |      
-      """;
+TicTacToeGame tic = new TicTacToeGame();
 
 
-Console.WriteLine(board);
+Console.WriteLine(tic.ToString());
--- TicTacToeBoard.cs
+++
@@ -1,6 +0,0 @@
-namespace ConsoleTicTacToe;
-
-public class TicTacToeBoard
-{
-    
-}
